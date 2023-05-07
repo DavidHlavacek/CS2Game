6e80d1ff6886211f4db85ff1637e88368e76b5a8
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2023-05-07 16:46:21 +0200

    Created tank enemie

---
+++ Base/Enemie.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace baseClasses
+{
+    public class Enemie : IPlayerOption
+    {
+        private string _name;
+        private int _level;
+        private int _health;
+        private int _damage;
+        private int _armor;
+        private double _speed;
+
+        public Enemie(string _name, int _level, int _health, int _damage, int _armor, double _speed)
+        {
+            this._name = _name;
+            this._level = _level;
+            this._health = _health;
+            this._damage = _damage;
+            this._armor = _armor;
+            this._speed = _speed;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                this._name = value;
+            }
+
+        }
+        
+        public int Level
+        {
+            get
+            {
+                return this._level;
+            }
+            set
+            {
+                this._level = value;
+            }
+        }
+        
+        public int Health
+        {
+            get
+            {
+                return this._health;
+            }
+            set
+            {
+                this._health = value;
+            }
+        }
+        
+        public int Damage
+        {
+            get
+            {
+                return this._damage;
+            }
+            set
+            {
+                this._damage = value;
+            }
+        }
+        
+        public int Armor
+        {
+            get
+            {
+                return this._armor;
+            }
+            set
+            {
+                this._armor = value;
+            }
+        }
+        
+        public double Speed
+        {
+            get
+            {
+                return this._speed;
+            }
+            set
+            {
+                this._speed = value;
+            }
+        }
+    }
+
+}
---
+++ Enemies/Troll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Base.Enemie;
+
+namespace Enemies
+{
+    public class Troll : Enemie
+    {
+        public Troll : base("Troll", 5, 200, 50, 100, 0.5)
+        {
+        }
+            
+        public void Attack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
